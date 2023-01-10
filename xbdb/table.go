dccf7080cba4d0241c9e891ac5c0046b812c8899
liaoran123 <2065551801@qq.com>
2023-01-10 16:34:13 +0800

    11

--- xbdb/table.go
+++ xbdb/table.go
@@ -4,6 +4,7 @@ package xbdb
 
 import (
 	"bytes"
+	"fmt"
 	"strconv"
 	"strings"
 	"sync"
@@ -206,8 +207,9 @@ func (t *Table) DataToJsonforIfo(tbd *TbData, Ifo *TableInfo) (r *bytes.Buffer) 
 		}
 		r.WriteString("{")
 		value = t.Split(v) //bytes.Split(v, []byte(Split))
-		if len(value) > len(Ifo.FieldType) {
-			break
+		if len(value) < len(Ifo.FieldType) {
+			fmt.Println("数据分解错误：", string(v), Ifo.FieldType)
+			continue
 		}
 		for i, fv := range Ifo.FieldType {
 			switch fv {
